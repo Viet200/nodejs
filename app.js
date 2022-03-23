bfecaae59af06cee5879148bdf8da8e5e71b327b
vietnvph13644 <vietnvph13644@fpt.edu.vn>
2022-03-23 22:12:13 +0700

    update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// include thư viện http
 import express from 'express';
 import homeRoute from './router/home';
 import productRoute from './router/products';
@@ -14,27 +13,6 @@ app.use(productRoute);
 app.use(categoryRoute);
 
 mongoose.connect('mongodb://127.0.0.1:27017/we16307');
-
-// khởi tạo server
-// const server = http.createServer((request,response)=>{
-//     console.log(request.url);
-//     if(request.url==="/"){
-//         response.setHeader("Content-Type","text/html");
-//         response.write("<h3>Home Page</h3>");
-//         response.end();
-//     } if(request.url==="/products"){
-//         response.setHeader("Content-Type","text/html");
-//         response.write("<h3>product Page</h3>");
-//         response.end();
-//     }
-//     if(request.url==="/news"){
-//         response.setHeader("Content-Type","text/html");
-//         response.write("<h3>News Page</h3>");
-//         response.end();
-//     }
-// });
-
-// Lắng nghe cổng thực thi
 
 const port = 3001;
 app.listen(port, () =>{
